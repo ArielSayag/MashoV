03c8e0be433fbc43c66513f87b66baed66a37b98
Ariela403 <52012874+Ariela403@users.noreply.github.com>
2020-06-12 14:03:37 +0300

    selectedIndex

--- mashovFinal/Models/DAL/DBservices.cs
+++ mashovFinal/Models/DAL/DBservices.cs
@@ -1335,7 +1335,7 @@ public class DBservices
 
         foreach (var item in allCrit)
         {
-            if (item.NumCrit == 0)
+            if ((!(item.NumCrit != 0))&&(!existingCrit(item)))
             {
 
 
